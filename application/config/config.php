d74d1d6db35f90b5f1e779d26867c0661e9328aa
faizramdhani <ramdhanifaiz7@gmail.com>
2023-01-23 08:48:04 +0700

    Set baseurl as dynamic

--- application/config/config.php
+++ application/config/config.php
@@ -30,7 +30,9 @@ defined('BASEPATH') or exit('No direct script access allowed');
 //         $_SERVER['SCRIPT_NAME']
 //     );
 
-$config['base_url'] = 'http://localhost/e-rekammedis/';
+$root = (isset($_SERVER['HTTPS']) ? "https://" : "http://") . $_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']), '', $_SERVER['SCRIPT_NAME']);
+$config['base_url'] = $root;
 
 /*
 |--------------------------------------------------------------------------
