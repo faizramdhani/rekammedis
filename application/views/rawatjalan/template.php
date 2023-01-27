7658ac2b5784c44f859f4532da4f0adb960921be
itrsmublitar <itrsmublitar@gmail.com>
2023-01-27 16:29:13 +0700

    change design poly nefrologi

--- application/views/rawatjalan/template.php
+++ application/views/rawatjalan/template.php
@@ -116,7 +116,7 @@
                                     <li><a class="nav-link" href="<?= site_url('Canak/index') ?>"><i class="fas fa-user-injured"></i> <span>Anak</span></a></li>
                                     <li><a class="nav-link" href="<?= site_url('Cbedahumum/index') ?>"><i class="fas fa-user-injured"></i> <span>Bedah Umum</span></a></li>
                                     <li><a class="nav-link" href="<?= site_url('Cobstetriginekologi/index') ?>"><i class="fas fa-user-injured"></i> <span>Obstetri&Ginekologi</span></a></li>
-                                    <li><a class="nav-link" href="<?= site_url('rawatjalan/nefrologi') ?>"><i class="fas fa-user-injured"></i> <span>Nefrologi</span></a></li>
+                                    <li><a class="nav-link" href="<?= site_url('Cnefrologi/index') ?>"><i class="fas fa-user-injured"></i> <span>Nefrologi</span></a></li>
                                     <li><a class="nav-link" href="<?= site_url('rawatjalan/jantung') ?>"><i class="fas fa-user-injured"></i> <span>Jantung & Pembuluh Darah</span></a></li>
                                     <li><a class="nav-link" href="<?= site_url('rawatjalan/kulitkelamin') ?>"><i class="fas fa-user-injured"></i> <span>Kulit & Kelamin</span></a></li>
                                     <li><a class="nav-link" href="<?= site_url('rawatjalan/mata') ?>"><i class="fas fa-user-injured"></i> <span>Mata</span></a></li>
