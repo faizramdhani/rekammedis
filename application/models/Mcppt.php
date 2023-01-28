81c542a786c7c0748b2fa41ddf56e94290fa6618
Moh Faiz Ramdhani <42823710+faizramdhani@users.noreply.github.com>
2023-01-28 23:30:22 +0700

    add C, M, V, form observasiigd anak dewasa

--- application/models/Mcppt.php
+++ application/models/Mcppt.php
@@ -7,7 +7,6 @@ class Mcppt extends CI_Model
     {
         $this->load->database();
     }
-<<<<<<< HEAD
     public function getcppt($noRM)
     {
         $query = $this->db->query("SELECT a.nomrm, a.nmpasien, c.nama, a.norj, b.kelamin, b.identitas, b.umurthn, a.kode1
@@ -17,15 +16,16 @@ class Mcppt extends CI_Model
         $result = $query->row_array();
         $this->session->set_userdata($result);
         return $result;
-=======
+    }
 
-    public function geticd(){
+    public function geticd()
+    {
 
         $query = $this->db->query("SELECT * FROM icds ORDER BY code ASC LIMIT 100");
 
         $result = $query->result_array();
         return $result;
         // return $query->result();
->>>>>>> 205847653b3bb8f402f7e5aa99723bddd8442bc9
+
     }
 }
